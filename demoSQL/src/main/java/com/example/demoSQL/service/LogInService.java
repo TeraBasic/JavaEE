c944a7141ede9e727df4fcbd1844d7ee453e3763
wyh713 <59499962+wyh713@users.noreply.github.com>
2020-01-08 11:55:33 +0100

    Update LogInService.java

--- demoSQL/src/main/java/com/example/demoSQL/service/LogInService.java
+++ demoSQL/src/main/java/com/example/demoSQL/service/LogInService.java
@@ -10,9 +10,25 @@ public class LogInService {
 	@Autowired
     private UserDao userDao;
 	
-	public boolean validateUser(Long id, String password) {
-		UserDo userDo = userDao.getOne(id);
+	public String  register(User user) {
+        if (userDao.getOneUser(user.getUsername()) == null) {
+            userDao.setOneUser(user);
+            return True;
+        }
+        else {
+            return False;
+        }
+    }
+	
+	public boolean validateUser(String username, String password) {
+		UserDo userDo = userDao.getOne(username);
+		if (dbUser == null) {
+            return False;
+        }
 		return userDo.getPwd().equals(password);
+		user.setusername(dbUser.getId());
+            user.setCreateTime(userDo.getCreateTime());
+            return "Login success";
 		
 	}
 
